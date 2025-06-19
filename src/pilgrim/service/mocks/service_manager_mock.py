4b301d0d7e762c60c288958bedb39737de5fa4f3
Gustavo Henrique Santos Souza de Miranda <gustavohssmiranda@gmail.com>
2025-06-19 03:22:10 -0300

    Added Async Operations for the model travel_diary_service_mock.py and service_manager_mock.py and added a call to main in the command.py

--- src/pilgrim/service/mocks/service_manager_mock.py
+++ src/pilgrim/service/mocks/service_manager_mock.py
@@ -8,12 +8,16 @@ from pilgrim.service.servicemanager import ServiceManager
 class ServiceManagerMock(ServiceManager):
     def __init__(self):
         super().__init__()
+        # Cria instâncias únicas para manter estado consistente
+        self._travel_diary_service = TravelDiaryServiceMock()
+        self._entry_service = EntryServiceMock()
+        self._photo_service = PhotoServiceMock()
 
     def get_entry_service(self):
-        return EntryServiceMock()
+        return self._entry_service
 
     def get_travel_diary_service(self):
-        return TravelDiaryServiceMock()
+        return self._travel_diary_service
 
     def get_photo_service(self):
-        return PhotoServiceMock()
+        return self._photo_service
