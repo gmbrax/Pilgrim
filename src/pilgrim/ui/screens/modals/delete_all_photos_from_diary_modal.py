40a2e727f8ac8d1c74fda3e38d9d27533c5cad3a
Gustavo Henrique Santos Souza de Miranda <gustavohssmiranda@gmail.com>
2025-07-23 03:00:49 -0300

    Add the modals to delete the diary and the diary contents

---
+++ src/pilgrim/ui/screens/modals/delete_all_photos_from_diary_modal.py
@@ -0,0 +1,20 @@
+
+from textual.widgets import Button
+
+from textual import on
+
+from pilgrim.ui.screens.modals.delete_yes_confirmation_modal import DeleteYesConfirmationModal
+
+
+class DeleteAllPhotosModal(DeleteYesConfirmationModal):
+    def __init__(self,diary_id:int):
+        super().__init__(diary_id)
+        self.head_text.update("Are you sure you want to delete all photos from this diary?")
+
+
+
+    @on(Button.Pressed, "#DeleteDiaryModal-DeleteButton")
+    def on_delete_button_pressed(self, event):
+        self.result = True
+        self.dismiss()
+        
